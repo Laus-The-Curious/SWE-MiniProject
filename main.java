6270491a41f3add19c0c8d07d42b5fd0273582b1
umang <umang.jaiswal2003@gmail.com>
2026-01-30 16:22:38 +0530

    uploading zero division exception code

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ import java.io.*;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int i = 10/0; // This will cause ArithmeticException
     }
 }
