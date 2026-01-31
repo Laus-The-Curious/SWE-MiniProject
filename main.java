fa8447c7490514a3062a3d368f4a5c3a2cec8004
umang <umang.jaiswal2003@gmail.com>
2026-01-31 11:28:56 +0530

    testing pull request 2

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ import java.io.*;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        // int i = 10/0; // This will cause ArithmeticException
+        int i = 10/0; // This will cause ArithmeticException
     }
 }
