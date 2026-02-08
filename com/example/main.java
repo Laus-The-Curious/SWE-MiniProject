cff7f00c344447388487c4fe6c24a81263bd8546
umang <umang.jaiswal2003@gmail.com>
2026-02-08 09:47:09 +0530

    passing Test 3

--- com/example/main.java
+++ com/example/main.java
@@ -1,8 +1,9 @@
 package com.example;
 class Main{
     public static void main(String[] args) {
-        Logger.log("Hello, World!");
+        Logger logger = new Logger();
+        logger.log("Hello, World!");
         int i = 10/1; // This will cause ArithmeticException
-        Logger.log(i); 
+        logger.log(i); 
     }
 }
